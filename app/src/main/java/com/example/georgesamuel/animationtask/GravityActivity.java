23e27545244aa4664460562db3605a7aeef07fcd
George <georgesamuel96>
2020-02-11 14:32:32 +0200

    move vertically with gravity

--- app/src/main/java/com/example/georgesamuel/animationtask/GravityActivity.java
+++ app/src/main/java/com/example/georgesamuel/animationtask/GravityActivity.java
@@ -12,8 +12,10 @@ public class GravityActivity extends AppCompatActivity {
     ImageView ball;
     ConstraintLayout container;
     private int speedX = 20, speedY = 20;
-    private int timeX = 1, timeY = 1, time = 1;
+    private int timeX = 1, timeY = 10, time = 1;
     private int pointX = 0, pointY = 0;
+    private int val = 0, slop = 1;
+    private int distance;
     final Handler handler = new Handler();
 
     @Override
@@ -29,5 +31,41 @@ public class GravityActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
+        runVertically();
+    }
+
+    private void runVertically() {
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                distance = container.getHeight() - (ball.getHeight() + val);
+                speedY = (int)getSpeed() * slop;
+                pointY += speedY;
+                ball.setX(pointX);
+                if(slop > 0)
+                    ball.setY(Math.min(container.getHeight() - ball.getHeight(), pointY));
+                else
+                    ball.setY(Math.max(val, Math.min(container.getHeight() - ball.getHeight(), pointY)));
+                if(isOut(pointX, pointY)){
+                    if(slop > 0) {
+                        val = Math.min(val + 50, container.getHeight() - ball.getHeight());
+                        timeY += 2;
+                    }
+//                    speedY *= -1;
+                    slop *= -1;
+//                    pointY += speedY;
+                }
+                handler.postDelayed(this, timeY);
+
+            }
+        }, 100);
+    }
+
+    private double getSpeed() {
+        return Math.sqrt(2.0 * 9.8 * (container.getHeight() - (ball.getHeight() + val)));
+    }
+
+    private boolean isOut(int x, int y) {
+        return (x < 0 || x > container.getWidth() - ball.getWidth() || y < val || y > container.getHeight() - ball.getHeight());
     }
 }
