031de77ee2f595ed75f3fff0e2ce659d377cb640
George <georgesamuel96>
2020-02-13 12:56:29 +0200

    vertically with gravity : change distance only

--- app/src/main/java/com/example/georgesamuel/animationtask/GravityActivity.java
+++ app/src/main/java/com/example/georgesamuel/animationtask/GravityActivity.java
@@ -12,7 +12,7 @@ public class GravityActivity extends AppCompatActivity {
     ImageView ball;
     ConstraintLayout container;
     private int speedX = 20, speedY = 20;
-    private int timeX = 1, timeY = 10, time = 1;
+    private int timeX = 1, timeY = 5, time = 1;
     private int pointX = 0, pointY = 0;
     private int val = 0, slop = 1;
     private int distance;
@@ -43,10 +43,11 @@ public class GravityActivity extends AppCompatActivity {
                 speedY = (int)getSpeed() * slop;
                 pointY += speedY;
                 ball.setX(pointX);
-                if(slop > 0)
-                    ball.setY(Math.min(container.getHeight() - ball.getHeight(), pointY));
-                else
-                    ball.setY(Math.max(val, Math.min(container.getHeight() - ball.getHeight(), pointY)));
+                ball.setY(
+                        (slop > 0)?
+                                Math.min(container.getHeight() - ball.getHeight(), pointY) :
+                                Math.max(val, Math.min(container.getHeight() - ball.getHeight(), pointY))
+                );
                 if(isOut(pointX, pointY)){
                     if(slop > 0) {
                         val = Math.min(val + 50, container.getHeight() - ball.getHeight());
@@ -70,5 +71,27 @@ public class GravityActivity extends AppCompatActivity {
 
     private boolean isOut(int x, int y) {
         return (x < 0 || x > container.getWidth() - ball.getWidth() || y < val || y > container.getHeight() - ball.getHeight());
+    }
+
+    private boolean isOut_(int x, int y) {
+        return (x < 0 || x > container.getWidth() - ball.getWidth() || y < val || y > container.getHeight() - ball.getHeight());
+    }
+
+    private void runVertically_() {
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                pointY += speedY;
+                if(isOut_(pointX, pointY)){
+                    speedY *= -1;
+                    pointY += speedY;
+                    val += (speedY < 0)? 50 : 0;
+                }
+                ball.setX(pointX);
+                ball.setY(pointY);
+                handler.postDelayed(this, timeY);
+
+            }
+        }, 100);
     }
 }
